9678412522141cc5a3a2af5322e02c8f39700823
郑伟业 <zwy@qq.com>
2023-10-17 21:11:11 +0800

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zhengweiye/test1
 
 go 1.19
+
+require github.com/zhengweiye/test2 v1.0.1 // indirect
--- test1/Test1.go
+++ test1/Test1.go
@@ -1,13 +1,17 @@
 package test1
 
+import "github.com/zhengweiye/test2/test2"
+
 type Test1 struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
 }
 
 func GetTest1() Test1 {
+	test2 := test2.GetTest2()
+
 	return Test1{
-		Id:   2,
-		Name: "lisi",
+		Id:   test2.Id + 10,
+		Name: "hello " + test2.Name,
 	}
 }
